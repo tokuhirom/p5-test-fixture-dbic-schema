6cab41bba6364179d9e759e6d806eb1501be0fb6
Steffen Schwigon <ss5@renormalist.net>
2010-01-20 04:15:13 +0100

    Prepare new CPAN release

--- lib/Test/Fixture/DBIC/Schema.pm
+++ lib/Test/Fixture/DBIC/Schema.pm
@@ -1,7 +1,7 @@
 package Test::Fixture::DBIC::Schema;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use base 'Exporter';
 our @EXPORT = qw/construct_fixture/;
 use Params::Validate ':all';
